fc4b22aefcb3b6dc38e61466b4e5ffaa01540893
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-05 00:17:39 +0600

    projects page created

---
+++ src/pages/projects.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const projects = () => {
+    return (
+        <>
+            <Head>
+                <title>Ashik Ahmed | About Page</title>
+                <meta name='description' content='ashik ahmed web developer. ashik ahmed full-stack developer. ashik daffodil'></meta>
+            </Head>
+        </>
+    );
+};
+
+export default projects;

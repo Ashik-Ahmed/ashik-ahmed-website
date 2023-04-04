153d37be4bddd2bbbd0b95101ed4f4b7efb88525
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-04 14:22:19 +0600

    about page image and animated number counting added

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,35 @@
 import AnimatedText from '@/Components/AnimatedText/AnimatedText';
 import Layout from '@/Components/Layout/Layout';
 import Head from 'next/head';
-import React from 'react';
+import Image from 'next/image';
+import React, { useEffect, useRef } from 'react';
+import profilePic from '../../public/images/profile/developer-pic-2.jpg'
+import { useInView, useMotionValue, useSpring } from 'framer-motion';
+
+const AnimatedMumbers = ({ value }) => {
+
+    const ref = useRef(null)
+
+    const motionValue = useMotionValue(0)
+    const springValue = useSpring(motionValue, { duration: 3000 })
+    const isInView = useInView(ref, { once: true })
+
+    useEffect(() => {
+        if (isInView) {
+            motionValue.set(value)
+        }
+    }, [isInView, value, motionValue])
+
+    useEffect(() => {
+        springValue.on("change", (latest) => {
+            if (ref.current && latest.toFixed(0) <= value) {
+                ref.current.textContent = latest.toFixed(0)
+            }
+        })
+    }, [springValue, value])
+
+    return <span ref={ref}></span>
+}
 
 const about = () => {
     return (
@@ -12,7 +40,7 @@ const about = () => {
             </Head>
             <main className='flex flex-col items-center justify-center w-full '>
                 <Layout className='pt-16'>
-                    <AnimatedText text='Passion Fuels Purpose!' />
+                    <AnimatedText text='Passion Fuels Purpose!' className='mb-16' />
                     <div className='grid w-full grid-cols-8 gap-16'>
                         <div className='col-span-3 flex flex-col items-start justify-start'>
                             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
@@ -26,6 +54,32 @@ const about = () => {
                                 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
                         </div>
 
+                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
+                            <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
+                            <Image src={profilePic} alt='ashik ahmed' className='w-full h-auto rounded-2xl' />
+                        </div>
+                        <div className='col-span-2 flex flex-col items-end justify-between'>
+                            <div className='flex flex-col items-end justify-center'>
+                                <span className='inine-block text-7xl font-bold'>
+                                    <AnimatedMumbers value={50} />+
+                                </span>
+                                <h2 className='text-xl font-medium capitalize text-dark/75'>satisfied clients</h2>
+                            </div>
+
+                            <div className='flex flex-col items-end justify-center'>
+                                <span className='inine-block text-7xl font-bold'>
+                                    <AnimatedMumbers value={40} />+
+                                </span>
+                                <h2 className='text-xl font-medium capitalize text-dark/75'>projects completed</h2>
+                            </div>
+
+                            <div className='flex flex-col items-end justify-center'>
+                                <span className='inine-block text-7xl font-bold'>
+                                    <AnimatedMumbers value={4} />+
+                                </span>
+                                <h2 className='text-xl font-medium capitalize text-dark/75'>years of experience</h2>
+                            </div>
+                        </div>
                     </div>
                 </Layout>
             </main>

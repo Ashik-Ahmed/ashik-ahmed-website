6aba44b49c488f3312f3c7ba63d0c6ce3783d57d
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-05 23:28:52 +0600

    Article ul section created

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -1,10 +1,38 @@
+import AnimatedText from '@/Components/AnimatedText/AnimatedText';
+import Layout from '@/Components/Layout/Layout';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+
+const FeaturedArticle = ({ img, title, time, summary, link }) => {
+    return (
+        <li>
+            <Link href={link} target='_blank' className='w-full cursor-pointer overflow-hidden rounded-lg'>
+                <Image src={img} alt={title} className='w-full h-auto' />
+            </Link>
+        </li>
+    )
+}
 
 const articles = () => {
     return (
-        <div>
-            Articles
-        </div>
+        <>
+            <Head>
+                <title>Ashik Ahmed | Articles Page</title>
+                <meta name='description' content='ashik ahmed web developer. ashik ahmed full-stack developer. ashik daffodil'></meta>
+            </Head>
+
+            <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden '>
+                <Layout className='pt-16'>
+                    <AnimatedText text="Words Can Change The World! " className='mb-16' />
+                    <ul className='grid grid-cols-2 gap-16'>
+                        <li>Featured Article-1</li>
+                        <li>Featured Article-2</li>
+                    </ul>
+                </Layout>
+            </main>
+        </>
     );
 };
 

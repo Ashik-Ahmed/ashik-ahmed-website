14554689ac9bade3a59d7383a8887937089c45a5
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-05 23:42:40 +0600

    second article image added

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -5,15 +5,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import article1 from '../../public/images/articles/pagination component in reactjs.jpg'
+import article2 from '../../public/images/articles/create loading screen in react js.jpg'
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
     return (
-        <li>
+        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
             <Link href={link} target='_blank' className='w-full cursor-pointer overflow-hidden rounded-lg'>
                 <Image src={img} alt={title} className='w-full h-auto' />
             </Link>
             <Link href={link} target='_blank'>
-                <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 hover:underline'>{title}</h2>
                 <p className='text-sm mb-2 '>{summary}</p>
                 <span className='text-primary font-semibold'>{time}</span>
             </Link>
@@ -41,7 +42,14 @@ const articles = () => {
                             link="/"
                             img={article1}
                         />
-                        <li>Featured Article-2</li>
+                        <FeaturedArticle
+                            title="Build A Custom Pagination Component In Reactjs From Scratch"
+                            summary=" Learn how to build a custom pagination component in ReactJS from scratch. 
+                   Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+                            time="9 min read"
+                            link="/"
+                            img={article2}
+                        />
                     </ul>
                 </Layout>
             </main>

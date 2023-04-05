10ad1fbdfa7d57ecde5724bd7b20035374a1ed74
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-05 23:37:54 +0600

    first article added

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -4,12 +4,18 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import article1 from '../../public/images/articles/pagination component in reactjs.jpg'
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
     return (
         <li>
             <Link href={link} target='_blank' className='w-full cursor-pointer overflow-hidden rounded-lg'>
                 <Image src={img} alt={title} className='w-full h-auto' />
+            </Link>
+            <Link href={link} target='_blank'>
+                <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+                <p className='text-sm mb-2 '>{summary}</p>
+                <span className='text-primary font-semibold'>{time}</span>
             </Link>
         </li>
     )
@@ -27,7 +33,14 @@ const articles = () => {
                 <Layout className='pt-16'>
                     <AnimatedText text="Words Can Change The World! " className='mb-16' />
                     <ul className='grid grid-cols-2 gap-16'>
-                        <li>Featured Article-1</li>
+                        <FeaturedArticle
+                            title="Build A Custom Pagination Component In Reactjs From Scratch"
+                            summary=" Learn how to build a custom pagination component in ReactJS from scratch. 
+                   Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+                            time="9 min read"
+                            link="/"
+                            img={article1}
+                        />
                         <li>Featured Article-2</li>
                     </ul>
                 </Layout>

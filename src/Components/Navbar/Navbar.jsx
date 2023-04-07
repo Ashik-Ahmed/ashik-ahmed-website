cf6e07df8bf0d309da772e87e2f4962bf17ffe39
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-07 22:16:59 +0600

    dark theme classes added on navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const CustomLink = ({ href, title, className = '' }) => {
     return (
         <Link href={href} className={`${className} relative group`}>
             {title}
-            <span className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+            <span className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'} dark:bg-light`}>&nbsp;</span>
         </Link>
     )
 }
@@ -64,7 +64,7 @@ const Navbar = () => {
                     <DribbbleIcon />
                 </motion.a>
 
-                <button onClick={() => setMode(mode == 'light' ? 'dark' : 'light')} className='ml-3 flex items-center justify-center rounded-full p-1'>
+                <button onClick={() => setMode(mode == 'light' ? 'dark' : 'light')} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
                     {
                         mode === 'dark' ?
                             <SunIcon className='fill-dark' />

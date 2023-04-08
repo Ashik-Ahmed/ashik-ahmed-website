5bb053a44263835de6eae3b3ed0b6e732ec0d267
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-08 23:39:17 +0600

    navbar hidden for small devices

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -31,61 +31,64 @@ const Navbar = () => {
     return (
         <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'>
 
-            <button className='flex flex-col items-center justify-center' onClick={handleClick}>
+            <button className='flex-col items-center justify-center hidden lg:flex' onClick={handleClick}>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
             </button>
 
-            <nav>
-                <CustomLink href='/' title='Home' className='mr-4' />
-                <CustomLink href='/about' title='About' className='mx-4' />
-                <CustomLink href='/projects' title='Projects' className='mx-4' />
-                <CustomLink href='/articles' title='Articles' className='mx-4' />
-            </nav>
+            <div className='w-full flex justify-between items-center lg:hidden'>
+                <nav>
+                    <CustomLink href='/' title='Home' className='mr-4' />
+                    <CustomLink href='/about' title='About' className='mx-4' />
+                    <CustomLink href='/projects' title='Projects' className='mx-4' />
+                    <CustomLink href='/articles' title='Articles' className='mx-4' />
+                </nav>
 
-            <nav className='flex items-center justify-center flex-wrap'>
-                <motion.a href='https://twitter.com' target='_blank'
-                    whileHover={{ y: -2 }}
-                    whileTap={{ scale: 0.9 }}
-                    className='w-6 mx-3'>
-                    <TwitterIcon />
-                </motion.a>
-                <motion.a href='/https://twitter.com' target='_blank'
-                    whileHover={{ y: -2 }}
-                    whileTap={{ scale: 0.9 }}
-                    className='w-6 mx-3'>
-                    <GithubIcon />
-                </motion.a>
-                <motion.a href='/' target='_blank'
-                    whileHover={{ y: -2 }}
-                    whileTap={{ scale: 0.9 }}
-                    className='w-6 mx-3'>
-                    <LinkedInIcon />
-                </motion.a>
-                <motion.a href='/' target='_blank'
-                    whileHover={{ y: -2 }}
-                    whileTap={{ scale: 0.9 }}
-                    className='w-6 mx-3'>
-                    <PinterestIcon className=' bg-light rounded-full' />
-                </motion.a>
-                <motion.a href='/' target='_blank'
-                    whileHover={{ y: -2 }}
-                    whileTap={{ scale: 0.9 }}
-                    className='w-6 ml-3'>
-                    <DribbbleIcon />
-                </motion.a>
+                <nav className='flex items-center justify-center flex-wrap'>
+                    <motion.a href='https://twitter.com' target='_blank'
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mx-3'>
+                        <TwitterIcon />
+                    </motion.a>
+                    <motion.a href='/https://twitter.com' target='_blank'
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mx-3'>
+                        <GithubIcon />
+                    </motion.a>
+                    <motion.a href='/' target='_blank'
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mx-3'>
+                        <LinkedInIcon />
+                    </motion.a>
+                    <motion.a href='/' target='_blank'
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mx-3'>
+                        <PinterestIcon className=' bg-light rounded-full' />
+                    </motion.a>
+                    <motion.a href='/' target='_blank'
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 ml-3'>
+                        <DribbbleIcon />
+                    </motion.a>
 
-                <button onClick={() => setMode(mode == 'light' ? 'dark' : 'light')} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
-                    {
-                        mode === 'dark' ?
-                            <SunIcon className='fill-dark' />
-                            :
-                            <MoonIcon className='fill-dark' />
-                    }
-                </button>
+                    <button onClick={() => setMode(mode == 'light' ? 'dark' : 'light')} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                        {
+                            mode === 'dark' ?
+                                <SunIcon className='fill-dark' />
+                                :
+                                <MoonIcon className='fill-dark' />
+                        }
+                    </button>
 
-            </nav>
+                </nav>
+            </div>
+
             <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
                 <Logo />
             </div>

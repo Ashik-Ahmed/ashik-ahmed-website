e689c25c6ba4e73fa6c3a8ec05e24a18df39c1e0
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-09 00:46:02 +0600

    homepage main section made responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,23 +17,23 @@ export default function Home() {
       </Head>
 
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-        <Layout className='pt-0'>
-          <div className="flex items-center justify-between w-full">
-            <div className='w-1/2'>
-              <Image src={profilePic} alt='ashik-ahmed-photo' className='w-full h-auto'
+        <Layout className='pt-0 md:pt-16 sm:pt-8'>
+          <div className="flex items-center justify-between w-full lg:flex-col">
+            <div className='w-1/2 md:w-full'>
+              <Image src={profilePic} alt='ashik-ahmed-photo' className='w-full h-auto lg:hidden md:inline-block md:w-full'
                 priority
                 sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               50vw"
               />
             </div>
-            <div className='w-1/2 flex flex-col items-center self-center'>
-              <AnimatedText text='Turning Vision Into Reality With Code And Design.' className='!text-5xl !text-left' />
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+              <AnimatedText text='Turning Vision Into Reality With Code And Design.' className='!text-5xl !text-left xl:!text-4xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl' />
               <p className='my-4 text-base font-medium'>
                 As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
-              <div className='flex items-center self-start mt-2'>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href='https://drive.google.com/file/d/1GdkNWggyO0Hc8Ik1y1cQ3rgj5Ki2Q1hY/view?usp=sharing' target='_blank'
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semobild hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light'
                   download={true}
@@ -48,7 +48,7 @@ export default function Home() {
 
         <HireMe />
 
-        <div className='absolute right-8 bottom-8 inline-block w-24'>
+        <div className='absolute right-8 bottom-8 inline-block w-24 md:hidden'>
           <Image src={lightBulb} alt='ashik' className='w-full h-auto ' />
         </div>
       </main>

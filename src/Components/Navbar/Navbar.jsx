8e9d714c05dc560f386aad6869cce6dcb39e4008
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-09 00:26:36 +0600

    mobile menubar design completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -29,9 +29,9 @@ const CustomMobileLink = ({ href, title, className = '', toggle }) => {
     }
 
     return (
-        <button onClick={handleClick} href={href} className={`${className} relative group text-light dark:text-dark`}>
+        <button onClick={handleClick} href={href} className={`${className} relative group text-light dark:text-dark my-2`}>
             {title}
-            <span className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'} dark:bg-light`}>&nbsp;</span>
+            <span className={`h-[2px] inline-block bg-light dark:bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
         </button>
     )
 }
@@ -66,25 +66,25 @@ const Navbar = () => {
                     <motion.a href='https://twitter.com' target='_blank'
                         whileHover={{ y: -2 }}
                         whileTap={{ scale: 0.9 }}
-                        className='w-6 mx-3'>
+                        className='w-6 mx-3 sm:mx-1'>
                         <TwitterIcon />
                     </motion.a>
                     <motion.a href='/https://twitter.com' target='_blank'
                         whileHover={{ y: -2 }}
                         whileTap={{ scale: 0.9 }}
-                        className='w-6 mx-3'>
+                        className='w-6 mx-3 sm:mx-1'>
                         <GithubIcon />
                     </motion.a>
                     <motion.a href='/' target='_blank'
                         whileHover={{ y: -2 }}
                         whileTap={{ scale: 0.9 }}
-                        className='w-6 mx-3'>
+                        className='w-6 mx-3 sm:mx-1'>
                         <LinkedInIcon />
                     </motion.a>
                     <motion.a href='/' target='_blank'
                         whileHover={{ y: -2 }}
                         whileTap={{ scale: 0.9 }}
-                        className='w-6 mx-3'>
+                        className='w-6 mx-3 sm:mx-1'>
                         <PinterestIcon className=' bg-light rounded-full' />
                     </motion.a>
                     <motion.a href='/' target='_blank'
@@ -110,7 +110,10 @@ const Navbar = () => {
             {/* Navbar for Mobile  */}
             {
                 isOpen ?
-                    <div className='min-w-[70vw] flex flex-col  justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
+                    <motion.div
+                        initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        className='min-w-[70vw] flex flex-col  justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
                         <nav className='flex items-center flex-col justify-center'>
                             <CustomMobileLink href='/' title='Home' className='' toggle={handleClick} />
                             <CustomMobileLink href='/about' title='About' className='' toggle={handleClick} />
@@ -118,35 +121,35 @@ const Navbar = () => {
                             <CustomMobileLink href='/articles' title='Articles' className='' toggle={handleClick} />
                         </nav>
 
-                        <nav className='flex items-center justify-center flex-wrap'>
+                        <nav className='flex items-center justify-center flex-wrap mt-2'>
                             <motion.a href='https://twitter.com' target='_blank'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
-                                className='w-6 mx-3'>
+                                className='w-6 mx-3 sm:mx-1'>
                                 <TwitterIcon />
                             </motion.a>
                             <motion.a href='/https://twitter.com' target='_blank'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
-                                className='w-6 mx-3'>
+                                className='w-6 mx-3 sm:mx-1 bg-light rounded-full dark:bg-dark'>
                                 <GithubIcon />
                             </motion.a>
                             <motion.a href='/' target='_blank'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
-                                className='w-6 mx-3'>
+                                className='w-6 mx-3 sm:mx-1'>
                                 <LinkedInIcon />
                             </motion.a>
                             <motion.a href='/' target='_blank'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
-                                className='w-6 mx-3'>
-                                <PinterestIcon className=' bg-light rounded-full' />
+                                className='w-6 mx-3 sm:mx-1 bg-light rounded-full'>
+                                <PinterestIcon />
                             </motion.a>
                             <motion.a href='/' target='_blank'
                                 whileHover={{ y: -2 }}
                                 whileTap={{ scale: 0.9 }}
-                                className='w-6 ml-3'>
+                                className='w-6 ml-3 sm:mx-1'>
                                 <DribbbleIcon />
                             </motion.a>
 
@@ -160,7 +163,7 @@ const Navbar = () => {
                             </button>
 
                         </nav>
-                    </div>
+                    </motion.div>
 
                     :
 

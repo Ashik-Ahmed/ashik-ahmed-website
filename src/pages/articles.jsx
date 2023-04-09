41e2461da632e143217852065a9f0e37b4988c01
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-10 00:40:12 +0600

    articles page now responsive as well as full website

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -38,7 +38,7 @@ const MovingImg = ({ title, img, link }) => {
                 style={{ x: x, y: y }}
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
-                ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg' />
+                ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden' />
         </Link>
     )
 }
@@ -50,9 +50,9 @@ const Article = ({ img, title, date, link }) => {
             initial={{ y: 150 }}
             whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
             viewport={{ once: true }}
-            className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid  border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light'>
+            className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid  border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light sm:flex-col'>
             <MovingImg title={title} img={img} link={link} />
-            <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
+            <span className='text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm'>{date}</span>
         </motion.li>
     )
 }
@@ -74,7 +74,7 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
                 />
             </Link>
             <Link href={link} target='_blank'>
-                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline xs:text-lg'>{title}</h2>
                 <p className='text-sm mb-2 '>{summary}</p>
                 <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>
             </Link>
@@ -92,8 +92,8 @@ const articles = () => {
 
             <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
                 <Layout className='pt-16'>
-                    <AnimatedText text="Words Can Change The World! " className='mb-16' />
-                    <ul className='grid grid-cols-2 gap-16'>
+                    <AnimatedText text="Words Can Change The World! " className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl' />
+                    <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:gap-y-16 md:grid-cols-1 '>
                         <FeaturedArticle
                             title="Build A Custom Pagination Component In Reactjs From Scratch"
                             summary=" Learn how to build a custom pagination component in ReactJS from scratch. 

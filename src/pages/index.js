5f7d30e5882f05c574b2b6bc8e9844f89bece3aa
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-10 00:26:56 +0600

    projects page now fully responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
               />
             </div>
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
-              <AnimatedText text='Turning Vision Into Reality With Code And Design.' className='!text-5xl !text-left xl:!text-4xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl' />
+              <AnimatedText text='Turning Vision Into Reality With Code And Design.' className='!text-5xl !text-left xl:!text-5xl lg:!text-center lg:!text-7xl md:!text-6xl sm:!text-4xl' />
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
                 As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.

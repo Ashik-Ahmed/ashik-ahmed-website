ab2fafef3c387b8adcccd935d68f6473420b3f43
ASHIK AHMED <ashikahmed121@gmail.com>
2023-04-12 11:42:05 +0600

    not rendering education component to identify vercel issue

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -111,7 +111,7 @@ const about = ({ about }) => {
 
                     <Skills skills={skills} />
                     <Experience experiences={experiences} />
-                    <Education educations={educations} />
+                    {/* <Education educations={educations} /> */}
                 </Layout>
             </main>
         </>

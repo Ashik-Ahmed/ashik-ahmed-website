89ef9fb5889d6ef66d7db5326c891409608e9bf8
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-17 13:39:12 +0600

    default theme changed to dark and title photo changed

--- database/connectMongo.js
+++ database/connectMongo.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const connectMongo = async () => mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connectMongo = async () => await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(error => console.log('Connection Error:', error));
 
 
